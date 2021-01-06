21b62fcf137d5fbe71b437bb8e6b828831ffb9f2
geol2 <big9401@gmail.com>
2021-01-06 23:06:52 +0900

    Update main-top (1)

---
+++ app/src/component/Main/Goods.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Goods.css'
+
+class Goods extends React.Component {
+    render() {
+        return (
+            <p> </p>
+        );
+    }
+}
+
+export default Goods;
--- app/src/component/Main/Main.js
+++ app/src/component/Main/Main.js
@@ -1,11 +1,25 @@
 import React from "react";
+import Goods from "./Goods";
 import './Main.css';
 
 class Main extends React.Component {
     render() {
         return (
             <div className="Main">
-                
+                <div className="Main-top"> 
+                    <div className="collection">
+                        <div className="collection subname">
+                            <p className="new"> NEW </p>
+                            <p> Wak </p>
+                            <p> Collection </p>
+                            <button className="seeall"> SEE ALL </button>
+                        </div>
+                        <div className="goods-component">
+                                <Goods>
+                                </Goods>
+                        </div> 
+                    </div>
+                </div>
             </div>
         );
     }
